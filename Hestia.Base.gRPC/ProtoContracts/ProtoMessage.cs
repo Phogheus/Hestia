c8756ccd0a16ad8eb4af846b29bbc7cf4a471d32
Phogheus <coreywestcott@gmail.com>
2022-11-30 21:37:55 -0700

    Adding base grpc items and examples of standing such a service up

---
+++ Hestia.Base.gRPC/ProtoContracts/ProtoMessage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.Json;
+using ProtoBuf;
+
+namespace Hestia.Base.gRPC.ProtoContracts
+{
+    /// <summary>
+    /// Defines a generic protobuf wrapper for any kind of required message
+    /// </summary>
+    /// <typeparam name="T">Underlying type carried in <see cref="MessageBytes"/></typeparam>
+    [ProtoContract]
+    public class ProtoMessage<T>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Returns the serialized bytes of the underlying message object
+        /// </summary>
+        [ProtoMember(1, IsPacked = true, OverwriteList = true)]
+        public byte[] MessageBytes { get; init; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        private ProtoMessage()
+        {
+            MessageBytes = Array.Empty<byte>();
+        }
+
+        /// <summary>
+        /// Constructor taking the value to serialize and optional serializer options
+        /// </summary>
+        /// <param name="message">Message to serialize and transmit</param>
+        /// <param name="options">Options for serialization (default null)</param>
+        public ProtoMessage(T message, JsonSerializerOptions? options = null)
+        {
+            MessageBytes = JsonSerializer.SerializeToUtf8Bytes(message, options);
+        }
+
+        #endregion Constructors
+
+        #region Public Methods
+
+        /// <summary>
+        /// Deserializes and returns the underlying message
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public T? GetMessage(JsonSerializerOptions? options = null)
+        {
+            return (MessageBytes?.Length ?? 0) > 0
+                ? JsonSerializer.Deserialize<T>(MessageBytes, options)
+                : default;
+        }
+
+        #endregion Public Methods
+    }
+}
